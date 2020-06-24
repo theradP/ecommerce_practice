aceee9a507772daecf3828c6094c5acb34ff2041
theradP <mpranjal380@gmail.com>
2020-06-24 14:19:50 +0530

    first commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\pranjal\django_practice\django_project_boilerplate\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
